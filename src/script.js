278a5be0c1a8e19830338190a4b93359e7c13b12
Billy <billypatrick767@gmail.com>
2021-07-28 12:40:57 +0300

    added logic to fetch data

--- src/script.js
+++ src/script.js
@@ -1,5 +1,21 @@
 const Leaflet = require("leaflet");
 
+const searchField = document.getElementById("search-field");
+const inputForm = document.getElementById("input-form");
+
+const ValidIpAddressRegex =
+  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let searchValue = searchField.value;
+  if (ValidIpAddressRegex.test(searchValue)) {
+    fetchOnIp(searchValue);
+  } else {
+    fetchOnDomain(searchValue);
+  }
+});
+
 //const myMap = document.getElementById("my-map");
 const accessToken =
   "pk.eyJ1IjoiZGV2YmlsbHkiLCJhIjoiY2tybjU2MWg1MHRyZDJybnZwdzBkbjJ1ZSJ9.wpkz8I0D7veTwCUjANha0Q";
@@ -16,3 +32,35 @@ L.tileLayer(
     accessToken: accessToken,
   }
 ).addTo(myMap);
+
+function fetchOnIp(ipAddress) {
+  const API_KEY = "at_QY2m6gKaAP8QJGKvKv64W9vlgsYgk";
+  let requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ipAddress}`,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
+
+function fetchOnDomain(domain) {
+  const API_KEY = "at_QY2m6gKaAP8QJGKvKv64W9vlgsYgk";
+  let requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(
+    `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&domain=${domain}`,
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
